af3abb61b1b5694caad60ebc1a7094dae1dcf9af
Sean Cascketta <scascketta@gmail.com>
2015-08-04 00:17:17 -0500

    Refactor slice script

--- tools/slice.go
+++ tools/slice.go
@@ -20,31 +20,19 @@ type VehicleLocationCollection struct {
 	Data      []agency.VehicleLocation `json:"data"`
 }
 
-func GetData(dbPath, dest string, min int64, max int64) error {
-	minStr, maxStr := strconv.FormatInt(min, 10), strconv.FormatInt(max, 10)
-	minIso, maxIso := time.Unix(min, 0).Format(Iso8601Format), time.Unix(max, 0).Format(Iso8601Format)
-	log.Printf("Get data between %s and %s\n", minIso, maxIso)
-
-	log.Println("dbPath: ", dbPath)
-	db, err := bolt.Open(dbPath, 0600, nil)
-	if err != nil {
-		return err
-	}
-
+func readBoltData(db *bolt.DB, min, max string) (*[]agency.VehicleLocation, error) {
 	locations := []agency.VehicleLocation{}
 
-	err = db.View(func(tx *bolt.Tx) error {
+	err := db.View(func(tx *bolt.Tx) error {
 		topBucket := tx.Bucket([]byte(capmetro.BucketName))
 
-		err = topBucket.ForEach(func(tripID, _ []byte) error {
+		err := topBucket.ForEach(func(tripID, _ []byte) error {
 			tripBucket := topBucket.Bucket(tripID)
 			c := tripBucket.Cursor()
 
-			for k, v := c.Seek([]byte(minStr)); k != nil && bytes.Compare(k, []byte(maxStr)) <= 0; k, v = c.Next() {
-				pbData := v
-
+			for k, v := c.Seek([]byte(min)); k != nil && bytes.Compare(k, []byte(max)) <= 0; k, v = c.Next() {
 				var loc agency.VehicleLocation
-				if err = proto.Unmarshal(pbData, &loc); err != nil {
+				if err := proto.Unmarshal(v, &loc); err != nil {
 					return err
 				}
 
@@ -57,31 +45,50 @@ func GetData(dbPath, dest string, min int64, max int64) error {
 		return err
 	})
 
-	if err != nil {
-		return err
-	}
+	return &locations, err
+}
 
-	log.Printf("Writing %d vehicle locations to %s.\n", len(locations), dest)
+func writeData(dest, min, max string, locations *[]agency.VehicleLocation) error {
+	log.Printf("Writing %d vehicle locations to %s.\n", len(*locations), dest)
 
 	coll := VehicleLocationCollection{
-		Count:     len(locations),
-		StartDate: minIso,
-		EndDate:   maxIso,
-		Data:      locations,
+		Count:     len(*locations),
+		StartDate: min,
+		EndDate:   max,
+		Data:      *locations,
 	}
 
 	f, err := os.Create(dest)
 	if err != nil {
-		log.Println(err)
 		return err
 	}
 	defer f.Close()
 
 	enc := json.NewEncoder(f)
 	if err = enc.Encode(coll); err != nil {
-		dlog.Println(err)
+		log.Println(err)
 		return err
 	}
 
 	return nil
+}
+
+func GetData(dbPath, dest string, min int64, max int64) error {
+	minStr, maxStr := strconv.FormatInt(min, 10), strconv.FormatInt(max, 10)
+	minIso, maxIso := time.Unix(min, 0).Format(Iso8601Format), time.Unix(max, 0).Format(Iso8601Format)
+	log.Printf("Get data between %s and %s\n", minIso, maxIso)
+
+	log.Println("dbPath: ", dbPath)
+	db, err := bolt.Open(dbPath, 0600, nil)
+	if err != nil {
+		return err
+	}
+
+	locations, err := readBoltData(db, minStr, maxStr)
+	if err != nil {
+		return err
+	}
+
+	err = writeData(dest, minIso, maxIso, locations)
+	return err
 }
